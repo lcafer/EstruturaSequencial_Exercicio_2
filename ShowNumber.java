d80770a4adf44057c086f0b5e7de7dde6741f025
Lucas Ferreira <>
2023-04-07 20:10:24 -0300

    Alterado código, foi do exercício errado.

--- ShowNumber.java
+++ ShowNumber.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class ShowNumber {
 	public static void main (String[] args) {
 		Scanner getInput = new Scanner(System.in);
-		System.out.println("Me diga seu nome:");
-		String getName = getInput.nextLine();
-		System.out.println("Olá, " + getName);
+		System.out.println("Insira um número");
+		double getNumber = getInput.nextDouble();
+		System.out.printf("O número informado foi {%f}.", getNumber);
 		getInput.close();
 	}
 }
